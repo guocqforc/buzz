4d1b4d6c4c9238f3ddbd8d1eff0815f9860c4f73
dantezhu <dantezhu@qq.com>
2015-01-04 22:06:41 +0800

    告警居然搞定

--- buzz_web/frontend/views.py
+++ buzz_web/frontend/views.py
@@ -92,15 +92,17 @@ def send_alarm(request):
     for person in config.notify_persons.all():
         receivers.add(person.email)
 
-    content = 'stat_name: %s\n' % config['stat_name']
+    content = 'stat_name: %s\n' % config.stat_name
 
     if json_data['number_value'] is not None:
         # 说明是相关的
-        content += '%s %s %s\n' % (json_data['number_value'], config.number_cmp, config.number_value)
+        content += 'number_value: %s %s %s\n' % (json_data['number_value'], config.number_cmp, config.number_value)
 
     if json_data['slope_value'] is not None:
         # 说明是相关的
-        content += '%s %s %s\n' % (json_data['slope_value'], config.slope_cmp, config.slope_value)
+        content += 'slope_value: %s %s %s\n' % (json_data['slope_value'], config.slope_cmp, config.slope_value)
+
+    logger.debug('content: %s', content)
 
     send_mail(receivers, settings.ALARM_EMAIL_SUBJECT, content)
 
