a25d4c7ed8b7463b5ae43b52ca33804ac3e9e8f0
dantezhu <dantezhu@qq.com>
2015-03-28 18:12:21 +0800

    用number_value也对

--- buzz_agent/buzz_agent/buzz_agent.py
+++ buzz_agent/buzz_agent/buzz_agent.py
@@ -79,13 +79,14 @@ class BuzzAgent(object):
 
                     alarm_benchmark += 1
 
-                    if v is not None:
-                        code = '%s %s %s' % (v, conf['number_cmp'], conf['number_value'])
+                    number_value = v
+                    if number_value is not None:
+                        code = '%s %s %s' % (number_value, conf['number_cmp'], conf['number_value'])
                         if eval(code):
                             alarm_num += 1
 
                             # 命中才给值
-                            hit_number_value = v
+                            hit_number_value = number_value
 
                 if conf['slope_cmp'] is not None and conf['slope_value'] is not None:
                     # 斜率
