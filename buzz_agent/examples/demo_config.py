464eea173ce7b086685c30258eb6312a05265513
dantezhu <dantezhu@qq.com>
2016-11-07 16:56:17 +0800

    LOGGING

--- buzz_agent/examples/demo_config.py
+++ buzz_agent/examples/demo_config.py
@@ -4,6 +4,46 @@ STAT_PATH = '/opt/graphite/storage/whisper/'
 
 DOMAIN = 'buzz.example.com'
 
-SECRET = 'ser'
+SECRET = 'secret'
 
 INTERVAL = 60
+
+LOG_FORMAT = '\n'.join((
+    '/' + '-' * 80,
+    '[%(levelname)s][%(asctime)s][%(process)d:%(thread)d][%(filename)s:%(lineno)d %(funcName)s]:',
+    '%(message)s',
+    '-' * 80 + '/',
+))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'standard': {
+            'format': LOG_FORMAT,
+        },
+    },
+
+    'handlers': {
+        'flylog': {
+            'level': 'ERROR',
+            'class': 'flylog.LogHandler',
+            'formatter': 'standard',
+            'source': 'buzz',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+
+    'loggers': {
+        'buzz': {
+            'handlers': ['console', 'flylog'],
+            'level': 'ERROR',
+            'propagate': False
+        },
+    }
+}
