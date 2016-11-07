b20a9fe1653ae52c8eab7e998b776b4a672d8c88
dantezhu <dantezhu@qq.com>
2016-11-07 20:53:28 +0800

    版本升级

--- buzz_agent/setup.py
+++ buzz_agent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="buzz_agent",
-    version='1.1.21',
+    version='1.1.25',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     zip_safe=False,
     platforms='any',
